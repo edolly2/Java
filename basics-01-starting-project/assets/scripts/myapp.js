40cc85e3d1f3793b31a0151e47ab513c630c7b1a
edolly2 <edolly2@protonmail.com>
2021-02-02 02:26:49 -0600

    "First assignment"

--- basics-01-starting-project/assets/scripts/myapp.js
+++ basics-01-starting-project/assets/scripts/myapp.js
@@ -1,1 +1,7 @@
-alert('This works!');
+let currentResult = 0;
+
+currentResult = currentResult + 10;
+
+let calcDescription = '0 + 10';
+
+outputResult(currentResult, calcDescription);
---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,11 @@
+const userInput = 2;
+let userResult;
+
+userResult = 18 + userInput;
+
+userResult = userResult - 16;
+userResult = userResult * 16;
+userResult = userResult / 16;
+
+alert(userInput);
+alert(userResult);
